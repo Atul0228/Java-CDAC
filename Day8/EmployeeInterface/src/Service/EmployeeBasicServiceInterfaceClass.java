32ab9d2088d13241efc499f177e9ce4bd44d2203
Atul0228 <atulyadav229000@gmail.com>
2025-10-09 12:33:50 +0530

    Add files via upload

---
+++ Day8/EmployeeInterface/src/Service/EmployeeBasicServiceInterfaceClass.java
@@ -0,0 +1,21 @@
+package Service;
+
+import java.util.List;
+
+import Beans.Employe;
+
+public interface EmployeeBasicServiceInterfaceClass {
+	boolean addNewEmployeeService();
+
+	List<Employe> displayAllData();
+
+	Employe searchById(int sid);
+
+	Employe searchByName(String sName);
+
+	boolean updateSalary(int id, double uSalary);
+
+	boolean deleteById(int id);
+
+	Employe searchBySalary(double sSalary);
+}
